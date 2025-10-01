efecbd8794b373ef42de986f21e075e452200971
digger <digger@mysmf.net>
2025-10-01 07:10:21 +0400

    Addlang strings for admin area mod settings.

--- Themes/default/languages/WhoDownloaded/WhoDownloaded.english.php
+++ Themes/default/languages/WhoDownloaded/WhoDownloaded.english.php
@@ -12,3 +12,7 @@ $txt['attachment_download_list'] = 'Members list';
 $txt['attachment_download_list_empty'] = 'The list is empty.';
 $txt['permissionname_show_download_list'] = 'View the list who downloaded attachments';
 $txt['permissionhelp_show_download_list'] = 'Can view the list of members who downloaded any attachment.';
+
+$txt['who_downloaded_settings_title'] = 'Who Downloaded Attachment Settings';
+$txt['who_downloaded_cache_time'] = 'Cache time for downloaders list (seconds)';
+$txt['who_downloaded_cache_time_desc'] = 'How long to cache the list of who downloaded an attachment. Set 0 to disable caching.';
