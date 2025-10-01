1226a752bb7dd116f4231af3e0f05af4b112efd6
digger <digger@mysmf.net>
2025-10-01 07:14:48 +0400

    Update lang strings for admin area mod settings.

--- Themes/default/languages/WhoDownloaded/WhoDownloaded.russian-utf8.php
+++ Themes/default/languages/WhoDownloaded/WhoDownloaded.russian-utf8.php
@@ -12,3 +12,7 @@ $txt['attachment_download_list'] = 'Скачали';
 $txt['attachment_download_list_empty'] = 'Список пуст.';
 $txt['permissionname_show_download_list'] = 'Просмотр списка скачавших вложение';
 $txt['permissionhelp_show_download_list'] = 'Разрешить пользователям этой группы просмотр списка скачавших каждое вложение.';
+
+$txt['who_downloaded_settings_title'] = 'Настройки мода Who Downloaded Attachment';
+$txt['who_downloaded_cache_time'] = 'Время кеширования списка скачавших (в секундах)';
+$txt['who_downloaded_cache_time_desc'] = 'Сколько кешировать список скачавших. Установите 0 чтобы выключить кеширование.';
