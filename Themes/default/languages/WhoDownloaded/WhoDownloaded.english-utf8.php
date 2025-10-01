79bf75ba87fd18a60be48c58571b7bb4e2ff78a0
digger <digger@mysmf.net>
2025-10-01 07:20:54 +0400

    Add filter by date for memvers list.

--- Themes/default/languages/WhoDownloaded/WhoDownloaded.english-utf8.php
+++ Themes/default/languages/WhoDownloaded/WhoDownloaded.english-utf8.php
@@ -16,3 +16,6 @@ $txt['permissionhelp_show_download_list'] = 'Can view the list of members who do
 $txt['who_downloaded_settings_title'] = 'Who Downloaded Attachment Settings';
 $txt['who_downloaded_cache_time'] = 'Cache time for downloaders list (seconds)';
 $txt['who_downloaded_cache_time_desc'] = 'How long to cache the list of who downloaded an attachment. Set 0 to disable caching.';
+
+$txt['who_downloaded_max_days'] = 'Show downloads from the last X days';
+$txt['who_downloaded_max_days_desc'] = 'Set the number of days to display downloads. Set 0 to show all.';
